b01a692bedbf06bbbe7deb8d5e8c861277fce20a
Jaquim Tchuculia <joaquimmarianotchuculia@gmail.com>
2025-09-08 19:25:13 +0100

    eliminei

--- exercicios/ex003/script.js
+++ exercicios/ex003/script.js
@@ -1,11 +1,13 @@
+
+
 function carregar(){
 
 var msg = document.getElementById('msg')
 var img = document.getElementById('imagem')
 var data = new Date()
 //var hora = data.getHours()
+var hora = 15
 
-var hora = 12
 msg.innerHTML = `Agora sao ${hora} horas`
 if(hora >= 0 && hora <=12){
     img.src = 'back-phone.jpg'
@@ -25,3 +27,5 @@ if(hora >= 0 && hora <=12){
 
 
 }
+
+
