94c5c7efa6c5183dd6debcd1db7f8619bcf3223d
Jaquim Tchuculia <joaquimmarianotchuculia@gmail.com>
2025-10-04 22:17:54 +0100

    nova actualizacao

---
+++ exercicios/projecto-chikila/js/scriptlog.js
@@ -0,0 +1,41 @@
+function entrar() {
+    let nav = document.querySelector('nav')
+    let h1 = document.getElementById('h1')
+    let img = document.querySelector('p.img')
+    nav.style.display='block'
+    h1.style.display='none'
+    img.style.position='center'
+
+}
+function sair() {
+    let nav = document.querySelector('nav')
+    let h1 = document.getElementById('h1')
+    let img = document.querySelector('p.img')
+    nav.style.display='none'
+    h1.style.display='block'
+    
+}
+
+function inscrever() {
+   let fnome = document.getElementById('nome') 
+   
+   let fnum = document.getElementById('num')
+   let res = document.getElementById('res')
+   res.style.color='skyblue'
+   res.style.font='bold'
+   res.style.font='Arial'
+   //novas declaraçoes
+   let nome = fnome.value
+   let num = fnum.value
+  // validar os campos
+  if(nome.length ==0 || num.length==0){
+   res.innerHTML= 'Operaçao nulla, tens de preencher todos os campos!'
+  } else{
+    res.innerHTML= `Bem-vindo de volta`
+    
+  }
+
+
+
+
+}
